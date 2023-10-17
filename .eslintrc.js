4d91d909834d96bb7a8030133ce547a190a7c266
priteev <alexander.priteev@sibers.com>
2023-10-17 18:05:21 +0600

    fix: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,33 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'import'],
   extends: ['airbnb-base'],
-  rules: {},
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts','.d.ts', '.tsx'],
+        moduleDirectory: ['src', 'node_modules']
+      }
+    }
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.spec.ts',
+      ],
+      env: {
+        jest: true,
+        browser: true,
+      }
+    }
+  ]
 };
