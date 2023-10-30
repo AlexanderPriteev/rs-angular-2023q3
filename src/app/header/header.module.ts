ec5eb003daf4fc34dc11cc8d3e334ee19554986a
priteev <garsteera@gmail.com>
2023-10-31 02:24:54 +0600

    feat: add standalone component

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { LogoComponent } from './logo/logo.component';
 import { SearchModule } from './search/search.module';
 import { SortModule } from './sort/sort.module';
 import { UserInfoModule } from './user-info/user-info.module';
@@ -15,6 +16,7 @@ import { UserInfoModule } from './user-info/user-info.module';
     SearchModule,
     SortModule,
     UserInfoModule,
+    LogoComponent
   ],
   exports: [
     HeaderComponent,
---
+++ src/app/header/logo/logo.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="logo">
+      <img src="../../../assets/images/png/logo.png" alt="logo" class="logo__img">
+    </div>
+  `,
+  styleUrls: ['./logo.component.scss']
+})
+export class LogoComponent {
+
+}
