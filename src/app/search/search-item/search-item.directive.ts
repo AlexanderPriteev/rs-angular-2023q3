c085d5e3053fa8d7905877c191350b8f44d8f15e
priteev <garsteera@gmail.com>
2023-10-30 23:33:58 +0600

    feat: add "The colored border under the Search result item is implemented using a Directive"

---
+++ src/app/search/search-item/search-item.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appAgeColor]'
+})
+export class SearchItemDirective {
+  @Input() createdDate!: string;
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const addClass = (str: string) => this.renderer.addClass(this.el.nativeElement, str);
+    const currentDate = new Date().getTime();
+    const itemDate = new Date(this.createdDate).getTime();
+    const timeDifferenceInDays = (currentDate - itemDate) / (1000 * 3600 * 24);
+
+    if (timeDifferenceInDays > 180) {
+      addClass('search-item--older');
+    } else if (timeDifferenceInDays >= 30 && timeDifferenceInDays <= 180) {
+      addClass('search-item--old');
+    } else if (timeDifferenceInDays >= 7 && timeDifferenceInDays < 30) {
+      addClass('search-item--normal');
+    }
+  }
+}
--- src/app/search/search-item/search-item.module.ts
+++ src/app/search/search-item/search-item.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SearchItemComponent } from './search-item.component';
+import { SearchItemDirective } from './search-item.directive';
 
 @NgModule({
   declarations: [
     SearchItemComponent,
+    SearchItemDirective,
   ],
   imports: [
     CommonModule,
