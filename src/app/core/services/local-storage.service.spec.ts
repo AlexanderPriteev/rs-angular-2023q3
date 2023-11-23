c11dde9bca23aa8132e4537ab6ba9146960939ef
priteev <garsteera@gmail.com>
2023-11-23 16:39:50 +0600

    feat: add tests for LocalStorageService

--- src/app/core/services/local-storage.service.spec.ts
+++ src/app/core/services/local-storage.service.spec.ts
@@ -1,19 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-import { StoreModule } from '@ngrx/store';
+import {Store, StoreModule} from '@ngrx/store';
 
 import { LocalStorageService } from './local-storage.service';
+import {ISearchItem} from "../../youtube/interfaces/search-item.interface";
+import {testItem} from "../../youtube/companents/search-item/search-item.component.spec";
+import {addToFavorites} from "../../redux/actions/favorite.actions";
 
 describe('LocalStorageService', () => {
   let service: LocalStorageService;
+  let store: Store;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [StoreModule.forRoot({})]
     });
     service = TestBed.inject(LocalStorageService);
+    store = TestBed.inject(Store);
+    jest.spyOn(store, 'dispatch');
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should dispatch addToFavorites action for item in localStorage', () => {
+    const testData: ISearchItem[] = [testItem];
+    const getItemMock = jest.fn().mockReturnValue(JSON.stringify(testData));
+    Object.defineProperty(window, 'localStorage', { value: { getItem: getItemMock } });
+    service.initStoreFromLocalStorage();
+    expect(store.dispatch).toHaveBeenCalledTimes(testData.length);
+    expect(store.dispatch).toHaveBeenCalledWith(addToFavorites({ searchItem: testData[0] }));
   });
+
 });
