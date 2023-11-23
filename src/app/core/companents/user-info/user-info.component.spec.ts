eaa1146783113c4788ef34f78aed053fc8580f3e
priteev <garsteera@gmail.com>
2023-11-23 12:41:15 +0600

    feat: add tests for UserInfoComponent

--- src/app/core/companents/user-info/user-info.component.spec.ts
+++ src/app/core/companents/user-info/user-info.component.spec.ts
@@ -2,22 +2,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppRoutesModule } from '../../modules/routes.module';
 import { UserInfoComponent } from './user-info.component';
+import {LoginService} from "../../../auth/services/login.service";
+import {BehaviorSubject} from "rxjs";
+
+class MockLoginService {
+  currentUser = new BehaviorSubject<string>('test');
+  logout(): void {}
+}
 
 describe('UserInfoComponent', () => {
   let component: UserInfoComponent;
   let fixture: ComponentFixture<UserInfoComponent>;
+  let loginService: LoginService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [UserInfoComponent],
-      imports: [AppRoutesModule]
+      imports: [AppRoutesModule],
+      providers: [
+        { provide: LoginService, useClass: MockLoginService }
+      ]
     });
     fixture = TestBed.createComponent(UserInfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loginService = TestBed.inject(LoginService)
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call sortService', () => {
+    const triggerLogoutSpy = jest.spyOn(loginService, 'logout');
+    component.logout();
+    expect(triggerLogoutSpy).toBeCalled();
   });
 });
