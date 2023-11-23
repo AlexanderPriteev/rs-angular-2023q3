773a66e8e140c447763a58fb46db60afc3f23078
priteev <garsteera@gmail.com>
2023-11-23 16:14:01 +0600

    feat: add tests for HeaderComponent

--- src/app/core/pages/header/header.component.spec.ts
+++ src/app/core/pages/header/header.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoreModule } from '../../modules/core.module';
 import { HeaderComponent } from './header.component';
+import {NavigationEnd, Router} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -19,5 +22,23 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set initial values on ngOnInit', () => {
+    const router = TestBed.inject(Router);
+    const navigationEndEvent = new NavigationEnd(1, '/', '/');
+
+    (router.events as BehaviorSubject<NavigationEnd>).next(navigationEndEvent);
+    fixture.detectChanges();
+
+    expect(component.currentURL).toEqual('/');
+    expect(component.isMainRoute).toBeTruthy();
+  });
+
+  it('should toggle isSortVisible', () => {
+    const initialValue = component.isSortVisible;
+    component.toggleSort();
+    fixture.detectChanges();
+    expect(component.isSortVisible).toEqual(!initialValue);
   });
 });
