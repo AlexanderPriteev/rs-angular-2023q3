f4d4ff72d8e2947f0cc5d1c2404eed434f7fe376
priteev <garsteera@gmail.com>
2023-11-24 15:03:54 +0600

    feat: add tests for SearchItemDirective

--- src/app/youtube/directives/age-color.directive.spec.ts
+++ src/app/youtube/directives/age-color.directive.spec.ts
@@ -9,4 +9,38 @@ describe('SearchItemDirective', () => {
     const directive = new AgeColorDirective(elMock, rendererMock);
     expect(directive).toBeTruthy();
   });
+
+  describe('date class', () => {
+    let elMock: ElementRef;
+    let rendererMock: Renderer2;
+    let directive: AgeColorDirective;
+    let currentDate: Date;
+
+    beforeEach(() => {
+      elMock = {} as ElementRef;
+      rendererMock = {
+        addClass: jest.fn(),
+      } as unknown as Renderer2;
+      directive = new AgeColorDirective(elMock, rendererMock);
+      currentDate = new Date();
+    });
+    it('should add --older', () => {
+      currentDate.setDate(currentDate.getDate() - 181);
+      directive.createdDate = String(currentDate);
+      directive.ngAfterViewInit();
+      expect(rendererMock.addClass).toHaveBeenCalledWith(elMock.nativeElement, '--older');
+    });
+    it('should add --old', () => {
+      currentDate.setDate(currentDate.getDate() - 30);
+      directive.createdDate = String(currentDate);
+      directive.ngAfterViewInit();
+      expect(rendererMock.addClass).toHaveBeenCalledWith(elMock.nativeElement, '--old');
+    });
+    it('should add --normal', () => {
+      currentDate.setDate(currentDate.getDate() - 7);
+      directive.createdDate = String(currentDate);
+      directive.ngAfterViewInit();
+      expect(rendererMock.addClass).toHaveBeenCalledWith(elMock.nativeElement, '--normal');
+    });
+  });
 });
