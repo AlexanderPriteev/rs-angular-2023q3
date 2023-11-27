f4dba3ac6848ea0db7566ae65622d1089b5a9e03
priteev <garsteera@gmail.com>
2023-11-27 11:17:25 +0600

    feat: add tests for searchFilter pipe

--- src/app/youtube/pipes/search-filter.pipe.spec.ts
+++ src/app/youtube/pipes/search-filter.pipe.spec.ts
@@ -1,8 +1,34 @@
+import { testItem } from '../companents/search-item/search-item.component.spec';
+import { ISearchItem } from '../interfaces/search-item.interface';
 import { SearchFilterPipe } from './search-filter.pipe';
+
+const testResultsPipe = [
+  {
+    ...testItem,
+    snippet: { ...testItem.snippet, title: 'first' }
+  },
+  {
+    ...testItem,
+    snippet: { ...testItem.snippet, title: 'second' },
+  },
+  {
+    ...testItem,
+    snippet: { ...testItem.snippet, title: 'third' },
+  }
+] as ISearchItem[];
 
 describe('SearchResultPipe', () => {
   it('create an instance', () => {
     const pipe = new SearchFilterPipe();
-    expect(pipe).toBeTruthy();
+
+    let result = pipe.transform([], 'SEC');
+    expect(result).toEqual([]);
+
+    result = pipe.transform(testResultsPipe, '');
+    expect(result).toEqual(testResultsPipe);
+
+    result = pipe.transform(testResultsPipe, 'SEC');
+    expect(result.length).toEqual(1);
+    expect(result[0].snippet.title).toEqual('second');
   });
 });
