9cfe77e22d375fb206c46f49ffcd246e2e9f8aa0
priteev <garsteera@gmail.com>
2023-11-27 16:46:21 +0600

    feat: add tests for VideoStatsPipe

--- src/app/youtube/pipes/video-stats.pipe.spec.ts
+++ src/app/youtube/pipes/video-stats.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { VideoStatsPipe } from './video-stats.pipe';
+import {ISearchResults} from "../interfaces/search-result.interface";
+import {testResults} from "../pages/item-page/item-page.component.spec";
+import {of} from "rxjs";
 
 describe('VideoStatsPipe', () => {
   it('create an instance', () => {
     const pipe = new VideoStatsPipe();
-    expect(pipe).toBeTruthy();
+    const searchMock: ISearchResults = testResults;
+    const videoMock = [of(testResults)];
+
+    pipe.transform(searchMock, videoMock).subscribe((result) => {
+      expect(result.items).toBeDefined();
+    });
   });
 });
