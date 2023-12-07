3f442a08bac925e9d50cfac1d2efba51d02646ba
priteev <alexander.priteev@sibers.com>
2023-12-07 18:19:37 +0600

    feat: add logic for header

--- connections/src/app/header/components/user-info/user-info.component.ts
+++ connections/src/app/header/components/user-info/user-info.component.ts
@@ -1,7 +1,9 @@
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {NavigationEnd, Router, RouterModule} from '@angular/router';
 import {LogoutService} from "../../../auth/services/logout.service";
+import {filter} from "rxjs";
+import {AuthService} from "../../../auth/services/auth.service";
 
 @Component({
   selector: 'app-user-info',
@@ -11,7 +13,22 @@ import {LogoutService} from "../../../auth/services/logout.service";
   styleUrl: './user-info.component.scss'
 })
 export class UserInfoComponent {
-  constructor(public logoutService: LogoutService) {
+  currentURL = '/';
+  isAuth = false;
+  constructor(public logoutService: LogoutService,
+              private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event) => {
+      this.isAuth = !!this.authService.getToken();
+      if (event instanceof NavigationEnd) {
+        this.currentURL = event.url;
+      }
+    });
   }
 
 }
